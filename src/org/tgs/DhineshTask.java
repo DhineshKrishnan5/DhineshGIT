fb7d61fa3c264351fbde72b5ad5d95fe08f7c9d7
DhineshKrishnan5 <dhineshkrishnan5@gmail.com>
2023-02-01 14:11:18 +0530

    my task

---
+++ src/org/tgs/DhineshTask.java
@@ -0,0 +1,13 @@
+package org.tgs;
+
+public class DhineshTask {
+	public class DhineshNew {
+		public void ts1() {
+			System.out.println("ddgfdghfg");
+
+		}
+		public void ts2() {
+			System.out.println("dtgh");
+
+		}
+	}}
